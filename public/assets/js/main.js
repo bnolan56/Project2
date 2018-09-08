dbc1cb0ce364455692f18ec1fd271aa8d029e9e1
Brad Nolan <bnolan56@gmail.com>
2018-09-08 12:22:30 -0600

    blah

---
+++ public/assets/js/main.js
@@ -0,0 +1,12 @@
+$(function() {
+
+  $(".uk-search-input").keyup("click", function (e) {
+  e.preventDefault();
+  if (e.which === 13) {
+
+
+      // code to run get Search route
+  }
+  });
+
+});
