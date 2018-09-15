85c51d3b584a7d77f9818115da1b3aa1160e2c7e
Brad Nolan <bnolan56@gmail.com>
2018-09-15 00:42:40 -0600

    fixing final issues pagination and post

--- routes/homeroutes.js
+++ routes/homeroutes.js
@@ -3,64 +3,65 @@ const knex = require('../db/knex');
 var router = express.Router()
 
 
-
-router.get('/', function(req,res,next){
-    res.render('index');
+// Basic Page Routes
+router.get('/', function(req,res){
+  res.render('index');
 });
 
-router.get('/about', function(req,res,next){
-    res.render('about');
+router.get('/about', function(req,res){
+  res.render('about');
 });
 
-router.get('/contact', function(req,res,next){
-    res.render('contact');
+router.get('/contact', function(req,res){
+  res.render('contact');
 });
 
 //Search Route
 router.get('/search', function(req,res){
-    userchoice = "%" + req.query.userInput + "%";
-    const info = {
-        beerName: [],
+  userchoice = "%" + req.query.userInput + "%";
+  const info = {
+      beerName: [],
+  }
+  knex.select(
+      'beer_name',
+      'brewery_name',
+      'beer_style',
+      'id'
+  ).from('beers').where('beer_name','like',userchoice).orWhere('beer_style', 'like', userchoice).orWhere('brewery_name', 'like', userchoice).limit(10)
+  .then(function(results) {
+    for (var i = 0; i <results.length; i++){
+        info.beerName.push(results[i]);
     }
-    knex.select(
-        'beer_name',
-        'brewery_name',
-        'beer_style',
-        'id'
-    ).from('beers').where('beer_name','like',userchoice).orWhere('beer_style', 'like', userchoice).orWhere('brewery_name', 'like', userchoice).limit(10)
-    .then(function(results){
-        for (var i = 0; i <results.length; i++){
-            info.beerName.push(results[i]);
-        }
-        res.render('searchresults', info);
-    })
-  });
-
-  //DB route
-  router.get('/db', function(req,res){
-    
-    const info = {
-        beerName: [],
-    }
-    knex.select(
-        'beer_name',
-        'brewery_name',
-        'beer_style',
-        'id'
-    ).from('beers').limit(50)
-    .then(function(results){
-        for (var i = 0; i <results.length; i++){
-            info.beerName.push(results[i]);
-        }
-        res.render('viewdb', info);
-    })
-  });
+    res.render('searchresults', info);
+  })
+});
 
 
-  //Brewery Route
+//DB 1 - 5  routes
+router.get('/db', function(req,res){
+
+  const info = {
+      beerName: [],
+  }
+  knex.select(
+      'beer_name',
+      'brewery_name',
+      'beer_style',
+      'id'
+  ).from('beers').limit(50)
+  .then(function(results){
+      for (var i = 0; i <results.length; i++){
+          info.beerName.push(results[i]);
+      }
+      res.render('viewdb', info);
+  })
+});
+
+
+//Brewery Route
 router.get('/brewery/:id', function(req,res){
     const breweryChoice = '%'+req.params.id+'%';
-     
+
     const info = {
         beerName: [],
         beerData: []
@@ -79,40 +80,50 @@ router.get('/brewery/:id', function(req,res){
       'id'
     ).from('beers').where('brewery_name',"like", breweryChoice).limit(12)
       .then(function(results){
-         
+
         var stringy = JSON.stringify(results);
         var breweriez = JSON.parse(stringy);
         var goodtogo = breweriez[0];
         info.beerData.push(goodtogo);
         for (var i = 0; i<breweriez.length; i++){
             var beers = breweriez[i];
-            info.beerName.push(beers);   
+            info.beerName.push(beers);
         }
         res.render('brewery', info)
       })
   });
 
-  //Beer Route
-  router.get('/beer/:id', function(req,res){
-      const beerid = req.params.id
-      console.log(beerid)
-    knex.select(
-        'beer_name',
-        'beer_abv',
-        'beer_ibu',
-        'beer_style',
-        'beer_description',
-        'beer_logo',
-        'brewery_name',
-        'id'
-    ).from('beers').where('id',beerid)
-    .then(function(results){
-        var stringy = JSON.stringify(results);
-        var beerz = JSON.parse(stringy);
-        var goodtogo = beerz[0]
-        res.render('beer', goodtogo)
-    })
+//Beer Route
+router.get('/beer/:id', function(req,res){
+    const beerid = req.params.id
+    console.log(beerid)
+  knex.select(
+      'beer_name',
+      'beer_abv',
+      'beer_ibu',
+      'beer_style',
+      'beer_description',
+      'beer_logo',
+      'brewery_name',
+      'id'
+  ).from('beers').where('id',beerid)
+  .then(function(results){
+      var stringy = JSON.stringify(results);
+      var beerz = JSON.parse(stringy);
+      var goodtogo = beerz[0]
+      res.render('beer', goodtogo)
+  })
 });
 
+//Submit Brewery GET / POST Routes
+router.get('/submit', function(req,res){
+  res.render('submit');
+});
+
+router.post('/submit'), function(req,res){
+  let beerSubmission = req.body;
+  console.log(beerSubmission);
+  res.send(beerSubmission)
+}
 
 module.exports = router
