1a561d756241e55b842a1948e3ff76d78c1587f8
William Stearns <wcstearns@yahoo.com>
2018-09-16 20:39:04 -0600

    added the post routes for brewerys and beer and added a .env to protect database password

--- knexfile.js
+++ knexfile.js
@@ -1,10 +1,12 @@
+require("dotenv").config();
+
 module.exports = {
    development: {
        client: 'mysql',
        connection: {
            host: 'bbj31ma8tye2kagi.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
            user: 'hz672535ragvqiem',
-           password: 'utec2uop2kj90ywp',
+           password: process.env.DBPassword,
            database: 'cy4y8t572n0socmm'
        }
    },
